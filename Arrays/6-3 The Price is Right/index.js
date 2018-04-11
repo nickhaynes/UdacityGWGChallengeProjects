aa6e663a9df8ca4d9875426d59b44fd3a2bcf4ef
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-04-11 09:27:01 -0500

    Create index.js
    Directions:
    Starting with this array of prices, change the prices of the 1st, 3rd, and 7th elements in the array.
    
    var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
    TIP: The 1st element of any array has an index of 0.
    
    Afterwards, print out the prices array to the console.

---
+++ Arrays/6-3 The Price is Right/index.js
@@ -0,0 +1,11 @@
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ */
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+prices [0] = 14.92;
+prices [2] = 18.48;
+prices [6] = 194.18;
+
+console.log(prices);
