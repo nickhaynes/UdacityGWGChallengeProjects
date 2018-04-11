7bf5d95b272edcd5ac146fb49399644227e395c7
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-04-11 09:52:49 -0500

    Create app.js

---
+++ DOM Manipulation/Collecting Values/app.js
@@ -0,0 +1,15 @@
+/*
+For this quiz, use jQuery's val method to make live changes to the 'Cool Articles' <h1>!
+
+The starter code below creates an event listener that will run any time the input changes.
+For more on events, check the instructor notes.
+*/
+
+
+$('#input').on('change', function() {
+    var val;
+    val = $('#input').val();
+    var changeHeader;
+    changeHeader = $(".articles h1").first();
+    changeHeader.text(val);
+});
