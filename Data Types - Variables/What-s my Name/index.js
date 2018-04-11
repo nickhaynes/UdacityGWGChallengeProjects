1471808def48a977088918b4311814d9f93406fc
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-04-10 22:26:27 -0500

    Create index.js
    Directions:
    Create a variable called fullName and assign it your full name as a string.

---
+++ Data Types - Variables/What-s my Name/index.js
@@ -0,0 +1,9 @@
+/*
+ * Programming Quiz: What's my Name? (2-9)
+ */
+
+var firstName = "Not ";
+var middleName = "Telling ";
+var lastName = "You";
+var fullName = firstName + middleName + lastName
+console.log (fullName)
